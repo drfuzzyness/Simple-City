54dc7e68cddaaae0bd89078f700285e4dae77f5c
Matthew Conto <mbsconto@gmail.com>
2015-04-10 23:04:11 -0400

    Revert "Revert b3eb0c3..b112edc"
    This reverts commit 748e5bb42c6adb13f11ae58450fb11f803ef6144.

---
+++ Assets/RoadSegment.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RoadSegment : MonoBehaviour {
+
+	public TrafficNode nodeFront;
+	public TrafficNode nodeRight;
+	public TrafficNode nodeBack;
+	public TrafficNode nodeLeft;
+	public Transform sidewalkFront;
+	public Transform sidewalkRight;
+	public Transform sidewalkBack;
+	public Transform sidewalkLeft;
+
+	public Direction<TrafficNode> nodes;
+
+	public struct Direction<T> {
+		T forward;
+		T right;
+		T backward;
+		T left;
+	}
+
+
+
+
+	[Header("Config")]
+	public GameObject roadPrefab;
+	public float size;
+
+	public void buildRoadForward() {
+		GameObject newRoadGameObj = Instantiate( roadPrefab, transform.position + transform.forward * size, transform.rotation ) as GameObject;
+		RoadSegment newRoad = newRoadGameObj.GetComponent<RoadSegment>();
+		Debug.Log( newRoad );
+		nodeRight.nextNodes.Add( newRoad.nodeRight );
+		nodeRight.isConnected = true;
+		newRoad.nodeLeft.nextNodes.Add( nodeLeft );
+		sidewalkFront.gameObject.SetActive( false );
+		nodeLeft.isConnected = true;
+		newRoad.sidewalkBack.gameObject.SetActive( false );
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TrafficNode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrafficNode : MonoBehaviour {
+
+	public List<TrafficNode> nextNodes;
+	public Building accessableBuilding;
+
+	[Header("Status")]
+	public bool isConnected;
+
+	void OnDrawGizmos() {
+		if( nextNodes.Count >= 1 ) {
+			foreach( TrafficNode thisNode in nextNodes ) {
+				Gizmos.DrawLine( transform.position, thisNode.transform.position );
+			}
+		}
+		else {
+			Gizmos.DrawWireSphere( transform.position, .1f );
+		}
+	}
+}
