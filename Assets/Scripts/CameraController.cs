799dc4d76f11c0a94cf3ae8251fce564206f367e
Matthew Conto <mbsconto@gmail.com>
2015-04-30 16:43:31 -0400

    Can jet fuel melt steel beams?

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,18 @@ public class CameraController : MonoBehaviour {
 	public float cameraSpeed;
 	public Vector2 min; // -43.70458, -283.6006
 	public Vector2 max; // 200.8231, -42.18439
+	public bool clamp;
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate( Input.GetAxis("Horizontal") * cameraSpeed * Time.deltaTime,
 		                    0f,
 		                    Input.GetAxis("Vertical") * cameraSpeed * Time.deltaTime );
-		Vector3 pos = transform.position;
-		pos.x = Mathf.Clamp( transform.position.x, min.x, max.x );
-		pos.z = Mathf.Clamp( transform.position.z, min.y, max.y );
-		transform.position = pos;
+		if( clamp ) {
+			Vector3 pos = transform.position;
+			pos.x = Mathf.Clamp( transform.position.x, min.x, max.x );
+			pos.z = Mathf.Clamp( transform.position.z, min.y, max.y );
+			transform.position = pos;
+		}
 	}
 }
---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Car : MonoBehaviour {
+
+	public TrafficNode nextNode;
+	public TrafficNode previousNode;
+	public float timeBtwnNodes; // Units/sec
+
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator driveToNextNode() {
+		for( float time = 0f; time < timeBtwnNodes; time += Time.deltaTime ) {
+			transform.position = Vector3.Lerp( previousNode.transform.position,
+			                                  nextNode.transform.position,
+			                                  time / timeBtwnNodes );
+			transform.rotation = Quaternion.Lerp( previousNode.transform.rotation,
+			                                     nextNode.transform.rotation,
+			                                     time / timeBtwnNodes);
+			yield return null;
+		}
+		transform.position = nextNode.transform.position;
+		transform.rotation = nextNode.transform.rotation;
+		decideWhatNext();
+	}
+
+	public void decideWhatNext() {
+		previousNode = nextNode;
+		nextNode = chooseDestination( nextNode );
+		StartCoroutine( "driveToNextNode" );
+	}
+
+	TrafficNode chooseDestination( TrafficNode node ) {
+		int index = Random.Range( 0, node.nextNodes.Count );
+		Debug.Log ( gameObject + " is going to " + node.nextNodes[ index ] + " next" );
+		return node.nextNodes[ index ];
+	}
+}
